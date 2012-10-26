7c64019e45cb8d161f0b68fd1a8128a4de6259af
Kiall Mac Innes <kiall@managedit.ie>
2012-10-26 18:03:25 +0100

    Initial Commit - CLI Skeleton

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# Copyright 2012 Managed I.T.
+#
+# Author: Kiall Mac Innes <kiall@managedit.ie>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from setuptools import setup, find_packages
+from monikerclient.openstack.common import setup as common_setup
+
+install_requires = common_setup.parse_requirements(['tools/pip-requires'])
+tests_require = common_setup.parse_requirements(['tools/test-requires'])
+setup_require = common_setup.parse_requirements(['tools/setup-requires'])
+dependency_links = common_setup.parse_dependency_links([
+    'tools/pip-requires',
+    'tools/test-requires',
+    'tools/setup-requires'
+])
+
+setup(
+    name='monikerclient',
+    version='0.0',
+    description='DNS as a Service - Client',
+    author='Kiall Mac Innes',
+    author_email='kiall@managedit.ie',
+    url='https://launchpad.net/monikerclient',
+    packages=find_packages(exclude=['bin']),
+    include_package_data=True,
+    test_suite='nose.collector',
+    setup_requires=setup_require,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    dependency_links=dependency_links,
+    scripts=[
+        'bin/moniker',
+    ],
+    cmdclass=common_setup.get_cmdclass(),
+)
