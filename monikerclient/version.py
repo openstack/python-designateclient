72bc53b67cb3d8829247d1e631bdf7b198a4206e
Kiall Mac Innes <kiall@hp.com>
2013-05-25 13:07:04 +0100

    Migrate to PBR
    Change-Id: I4f32b7c3ca7831becdd49f75a7fa48bfcfa7fe76

--- monikerclient/version.py
+++ monikerclient/version.py
@@ -13,6 +13,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from monikerclient.openstack.common import version as common_version
+import pbr.version
 
-version_info = common_version.VersionInfo('python-monikerclient')
+version_info = pbr.version.VersionInfo('python-monikerclient')
