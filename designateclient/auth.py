b54b359a2e34f03fdee8e97533730a11d1f0e4be
abhishekkekane <abhishek.kekane@nttdata.com>
2016-01-30 01:09:00 -0800

    Python3: Add support for urlparse
    Replaced urlparse to use from six.moves.urllib2.urlparse.urlparse
    
    Change-Id: Ie1e0bd408333d613c057d8ecd92100b7cddb6340
    Partially-Implements: blueprint designate-py3

--- designateclient/auth.py
+++ designateclient/auth.py
@@ -13,10 +13,10 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from urlparse import urlparse
 
 from keystoneclient.v2_0.client import Client
 from requests.auth import AuthBase
+from six.moves.urllib.parse import urlparse
 
 
 class KeystoneAuth(AuthBase):
