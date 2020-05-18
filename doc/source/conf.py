2612ba13eb1daadf30e6fb8a4d5eb3458b752b5a
Andreas Jaeger <aj@suse.com>
2020-05-18 21:30:02 +0200

    Switch to newer openstackdocstheme and reno versions
    Switch to openstackdocstheme 2.2.0 and reno 3.1.0 versions. Using
    these versions will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I6bac230569a89c055115c852d5cc0cae027cddd8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,9 +9,9 @@
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'openstackdocstheme']
 
 # openstackdocstheme options
-repository_name = 'openstack/python-designateclient'
-bug_project = 'python-designateclient'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/python-designateclient'
+openstackdocs_bug_project = 'python-designateclient'
+openstackdocs_bug_tag = ''
 html_theme = 'openstackdocs'
 
 # Add any paths that contain templates here, relative to this directory.
@@ -24,25 +24,14 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'designateclient'
 copyright = u'2012, Managed I.T. 2013-2014, Hewlett-Packard Development Company, L.P.'
-
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-from designateclient.version import version_info as designateclient_version
-version = designateclient_version.canonical_version_string()
-# The full version, including alpha/beta/rc tags.
-release = designateclient_version.version_string_with_vcs()
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 modindex_common_prefix = ['designateclient']
--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -42,9 +42,10 @@ extensions = [
 ]
 
 # openstackdocstheme options
-repository_name = 'openstack/python-designateclient'
-bug_project = 'python-designateclient'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/python-designateclient'
+openstackdocs_bug_project = 'python-designateclient'
+openstackdocs_bug_tag = ''
+openstackdocs_auto_name = False
 html_theme = 'openstackdocs'
 
 # Add any paths that contain templates here, relative to this directory.
@@ -99,7 +100,7 @@ exclude_patterns = []
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
